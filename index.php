3fa1eb95a3c8f730bb554dc9ff48955bbaa6b8f0
Distr4ctio <m4rk.golding@gmail.com>
2015-03-23 18:44:14 +0200

    Testing Tabs

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
         <h1>Noob Design</h1>
         <p class="lead">Customer Care Made Easy</p>
 	</div>
+<!-- Tabs -->
+    <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+        <li class="active"><a href="#red" data-toggle="tab">Client</a></li>
+        <li><a href="#yellow" data-toggle="tab">Contact</a></li>
+        <li><a href="#green" data-toggle="tab">Products</a></li>
+        <li><a href="#blue" data-toggle="tab">Notes</a></li>
+        <li><a href="#pink" data-toggle="tab">Account</a></li>
+    </ul>
 <!-- Main Body -->
 	<div class="row">
 		<div class="col-md-4">This is the left</div>
