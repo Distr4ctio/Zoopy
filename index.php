12cdac72c5c154e0e2ed2f4d95ee0190089885e0
Distr4ctio <m4rk.golding@gmail.com>
2015-03-23 18:31:52 +0200

    bootstrap and Header

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <html>
- <h1> Company Name <h1>
+<!-- Bootstrap -->
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<!-- Header -->
+<div class="jumbotron">
+        <h1>Noob Design</h1>
+        <p class="lead">This is noob design</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+      </div>
+
 </html>
