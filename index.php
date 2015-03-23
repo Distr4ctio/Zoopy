a960149ed5f9d76b4e313ec6e9098b07af991c20
Distr4ctio <m4rk.golding@gmail.com>
2015-03-23 19:04:56 +0200

    css added to fill cols

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <!-- Bootstrap -->
 <link rel="stylesheet" href="./css/bootstrap.min.css">
+<link rel="stylesheet" href="./stylesheet.css">
 <!-- Header -->
 	<div class="jumbotron">
         <h1>Noob Design</h1>
@@ -9,14 +10,22 @@
 <!-- Tabs -->
     <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
         <li class="active"><a href="#red" data-toggle="tab">Client</a></li>
-        <li><a href="#yellow" data-toggle="tab">Contact</a></li>
+        <li><a href="#yellow" data-toggle="tab">Contacts</a></li>
         <li><a href="#green" data-toggle="tab">Products</a></li>
         <li><a href="#blue" data-toggle="tab">Notes</a></li>
         <li><a href="#pink" data-toggle="tab">Account</a></li>
     </ul>
 <!-- Main Body -->
 	<div class="row">
-		<div class="col-md-4">This is the left</div>
-        <div class="col-md-8">This is the right</div>
+		<div class="col-md-4">
+			<div class="clientfill">
+			The left
+			</div>
+		</div>
+        <div class="col-md-8">
+        	<div class="clientfill">
+			The Right
+			</div>
+        </div>
       </div>
 </html>
