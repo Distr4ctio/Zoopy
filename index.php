77d3d913150d8ce07db83a172a34d9d03a5b941f
Distr4ctio <m4rk.golding@gmail.com>
2015-03-25 23:53:41 +0200

    Left panel layout test 1

--- index.php
+++ index.php
@@ -3,23 +3,63 @@
 <link rel="stylesheet" href="./css/bootstrap.min.css">
 <link rel="stylesheet" href="./stylesheet.css">
 <!-- Header -->
-	<div class="jumbotron">
+	<div class="header">
         <h1>Noob Design</h1>
         <p class="lead">Customer Care Made Easy</p>
 	</div>
 <!-- Tabs -->
     <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
-        <li class="active"><a href="#red" data-toggle="tab">Client</a></li>
-        <li><a href="#yellow" data-toggle="tab">Contacts</a></li>
-        <li><a href="#green" data-toggle="tab">Products</a></li>
-        <li><a href="#blue" data-toggle="tab">Notes</a></li>
-        <li><a href="#pink" data-toggle="tab">Account</a></li>
+        <li class="active"><a href="#Client" data-toggle="tab">Client</a></li>
+        <li><a href="#Contacts" data-toggle="tab">Contacts</a></li>
+        <li><a href="#Products" data-toggle="tab">Products</a></li>
+        <li><a href="#Notes" data-toggle="tab">Notes</a></li>
+        <li><a href="#Account" data-toggle="tab">Account</a></li>
     </ul>
 <!-- Main Body -->
 	<div class="row">
+<!-- The left pane will contain details that are needed at first glance location details standing and primary contact -->
 		<div class="col-md-4">
 			<div class="clientfill">
-			The left
+<!-- This is the details of the clients company address location and account standing -->
+                <h4>Client Details</h4>
+                <table class="details">
+                    <tr>
+                        <td>Address</td>
+                        <td>282 Street Lane Randburg South Africa</td>
+                    </tr>
+                    <tr>
+                        <td>Co-ordinates</td>
+                        <td>-26.110604, 27.963414</td>
+                    </tr>
+                    <tr>
+                        <td>Account Standing</td>
+                        <td>Good</td>
+                    </tr>
+                </table>
+<!-- This will show the primary contacts details not all contacts thus the contacts tab is still needed -->
+                <h4>Contact Details</h4>
+                <table class="details">
+                    <tr>
+                        <td>First</td>
+                        <td>John</td>
+                    </tr>
+                    <tr>
+                        <td>Last</td>
+                        <td>Smith</td>
+                    </tr>
+                    <tr>
+                        <td>Cel</td>
+                        <td>1111111111</td>
+                    </tr>
+                    <tr>
+                        <td>Tel</td>
+                        <td>1111111111</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>email@email</td>
+                    </tr>
+                </table>
 			</div>
 		</div>
         <div class="col-md-8">
@@ -27,5 +67,5 @@
 			The Right
 			</div>
         </div>
-      </div>
+    </div>
 </html>
